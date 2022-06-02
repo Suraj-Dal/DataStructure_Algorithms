56af4052192cebedaededf21618946d3507b279e
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-03 00:38:24 +0530

    [Suraj]: Usecase 1 to find permutation of string is added.

---
+++ DSA/DSA/Algorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal class Algorithm
+    {
+        public void permutation(String str, int s, int e)
+        {
+           if (s == e - 1)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = s; i < e; i++)
+                {
+                    str = swap(str, s, i);
+                    permutation(str, s + 1, e);
+                    str = swap(str, s, i);
+                }
+            }
+        }
+        public string swap(string str, int s, int e)
+        {
+            char[] ch = str.ToCharArray();
+            char c;
+            c = ch[s];
+            ch[s] = ch[e];
+            ch[e] = c;
+            return string.Join("",ch);
+        }
+    }
+}
---
+++ DSA/DSA/Program.cs
@@ -0,0 +1,6 @@
+﻿DSA.Algorithm algo = new DSA.Algorithm();
+Console.WriteLine("Enter String:");
+string str = Console.ReadLine();
+int len = str.Length;
+Console.WriteLine("Permutations are:");
+algo.permutation(str, 0, len);

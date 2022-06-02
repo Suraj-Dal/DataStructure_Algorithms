3ab87109482ac0b6d3c48bf17862191cbb986223
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-03 02:08:19 +0530

    [Suraj]: Usecase 2 to binary search word in text file is added.

---
+++ DSA/DSA/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DSA
+{
+    internal class BinarySearch
+    {
+        public void search(string str)
+        {
+            string text = File.ReadAllText(@"D:\Suraj\Projects\DataStructure_Algorithms\binary.txt");
+            string[] words = text.Split(' ');
+            Array.Sort(words);
+            int s = 0, e = words.Length;
+            while (s <= e)
+            {
+                int mid = (s + e) / 2;
+                if (words[mid].Equals(str))
+                {
+                    Console.WriteLine("Word found in file.");
+                    return;
+                }
+                else if (words[mid].CompareTo(str) > 0)
+                    e = mid - 1;
+                else
+                    e = mid + 1;
+                Console.WriteLine("Word not found in file.");
+                return;
+            }
+            
+        }
+    }
+}
--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,6 +1,23 @@
-﻿DSA.Algorithm algo = new DSA.Algorithm();
-Console.WriteLine("Enter String:");
-string str = Console.ReadLine();
-int len = str.Length;
-Console.WriteLine("Permutations are:");
-algo.permutation(str, 0, len);
+﻿Console.WriteLine("1.Permutation\n2.Binary Search\nEnter your choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        DSA.Algorithm algo = new DSA.Algorithm();
+        Console.WriteLine("Enter String:");
+        string a = Console.ReadLine();
+        int len = a.Length;
+        Console.WriteLine("Permutations are:");
+        algo.permutation(a, 0, len);
+        break;
+    case 2:
+        DSA.BinarySearch binary = new DSA.BinarySearch();
+        Console.WriteLine("enter word to search:");
+        string str = Console.ReadLine();
+        binary.search(str);
+        break;
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
+

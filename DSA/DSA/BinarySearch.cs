903547e29621987bcddd30f2fd0d2137fb794814
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-03 09:49:40 +0530

    [Suraj]: Usecase 2 to binary search word in file is refactored.

--- DSA/DSA/BinarySearch.cs
+++ DSA/DSA/BinarySearch.cs
@@ -14,23 +14,26 @@ namespace DSA
             string text = File.ReadAllText(@"D:\Suraj\Projects\DataStructure_Algorithms\binary.txt");
             string[] words = text.Split(' ');
             Array.Sort(words);
+            int flag = 0;
             int s = 0, e = words.Length;
-            while (s <= e)
+            while (s < e - 1)
             {
+                
                 int mid = (s + e) / 2;
-                if (words[mid].Equals(str))
+                if (words[mid].Equals(str) || words[mid+1].Equals(str) || words[mid-1].Equals(str))
                 {
                     Console.WriteLine("Word found in file.");
                     return;
                 }
                 else if (words[mid].CompareTo(str) > 0)
                     e = mid - 1;
-                else
-                    e = mid + 1;
-                Console.WriteLine("Word not found in file.");
-                return;
+                else if (words[mid].CompareTo(str) < 0)
+                    s = mid + 1;
             }
-            
+            if (flag == 0)
+            {
+                Console.WriteLine("Word not found in file.");
+            }
         }
     }
 }
